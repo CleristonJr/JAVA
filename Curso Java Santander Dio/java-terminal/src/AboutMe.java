1d52ea4c2211c06369522edba8b2af58c77b28cd
CleristonJr <cleristonjr1999@gmail.com>
2024-04-29 18:35:39 -0300

    Utilização do Scanner para o usuário informar os argumentos

--- Curso Java Santander Dio/java-terminal/src/AboutMe.java
+++ Curso Java Santander Dio/java-terminal/src/AboutMe.java
@@ -1,12 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
 
     public static void main(String[] args) {
-        
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
 
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        //Imprimir resultados obtidos
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + "cm ");
