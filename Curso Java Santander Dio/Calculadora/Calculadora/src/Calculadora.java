0ab3986fc405d36f74866f1f91e4a9c6d0a9eec2
CleristonJr <cleristonjr1999@gmail.com>
2024-04-29 17:53:32 -0300

    Criação do projeto Calculadora e criação da documentação

---
+++ Curso Java Santander Dio/Calculadora/Calculadora/src/Calculadora.java
@@ -0,0 +1,28 @@
+/**
+ * <h1> Calculadora </h1>
+ * 
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe para desfrutaar dos recursos oferecidos pelo autor
+ * 
+ * @author Cleriston Jr.
+ * @version 1.0
+ * @since 29/04/2024
+ */
+
+ //No terminal execute o comando abaixo para criar a documentação:
+ //javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ..docs src/*.java
+
+public class Calculadora {
+
+    /**
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+
+     public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+     }
+    
+}
