0301ccac365692c605bada0b2063b1cc4432df63
CleristonJr <cleristonjr1999@gmail.com>
2024-04-30 19:48:14 -0300

    Tratamento de exceções

---
+++ Curso Java Santander Dio/tratamento-de-excecoes/src/AboutMe.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+
+        try {
+            
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        //Imprimir resultados obtidos
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close();
+        } catch (InputMismatchException e) {
+            System.err.println("Verifique os campos! Lembre-se que idade e altura devem ser numericos (em altura, coloque '.' no lugar da ',')");
+        }
+    }
+}
