44dc32b44fa3927a1d3267d6e204e87c5887f6c6
CleristonJr <cleristonjr1999@gmail.com>
2024-04-30 18:32:32 -0300

    Estruturas condicionais

---
+++ Curso Java Santander Dio/controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,18 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "M";
+
+        switch (plano) {
+            case "T": {
+                System.out.println("5Gb Youtube");
+            }
+            case "M":{
+                System.out.println("Whats e Instagram grátis");
+            }
+            case "B":{
+                System.out.println("100 minutos de ligação");
+            }
+        
+        }
+    }
+}
